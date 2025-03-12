64225142d2019fedc8d72ff72a7b63dac1def113
Bbshka12 <ilaturkevic5287@gmail.com>
2025-03-12 10:52:40 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        string pipeName = @"\\.\pipe\MyPipe";
+        NamedPipeClientStream pipeClient = new NamedPipeClientStream(".", pipeName, PipeDirection.Out);
+
+        try
+        {
+            // Подключаемся к серверу
+            pipeClient.Connect();
+            Console.WriteLine("Connected to server.");
+
+            using (BinaryWriter writer = new BinaryWriter(pipeClient))
+            {
+                Random random = new Random();
+
+                while (true)
+                {
+                    int number = random.Next(1, 101); // Случайное число от 1 до 100
+                    writer.Write(number);
+                    Console.WriteLine($"Sent {number} to server.");
+
+                    // Пауза в 1 секунду перед отправкой следующего числа
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("Server disconnected: " + e.Message);
+        }
+    }
+}
